f0ce39c2868d40ac5ae49694d3da01444b33b0fd
Steven Roach <stevenwroach@hotmail.com>
2018-08-06 07:53:51 -0700

    Update azuregfs.sh

--- azuregfs.sh
+++ azuregfs.sh
@@ -4,6 +4,40 @@ set -x
 
 USERNAME_ORG=${6}
 PASSWORD_ACT_KEY="${7}"
+SHAREDHOMESERVER=${8}
+
+SHARE_HOME=/share/home
+mkdir -p /share
+mkdir -p $SHARE_HOME
+
+# User
+HPC_USER=hpcuser
+HPC_UID=7007
+HPC_GROUP=hpc
+HPC_GID=7007
+
+
+setup_user()
+{  
+
+    yum -y install nfs-utils nfs-utils-lib
+
+	echo "$SHAREDHOMESERVER:$SHARE_HOME $SHARE_HOME    nfs4    rw,auto,_netdev 0 0" >> /etc/fstab
+	mount -a
+	mount
+   
+    groupadd -g $HPC_GID $HPC_GROUP
+
+    # Don't require password for HPC user sudo
+    echo "$HPC_USER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+    
+    # Disable tty requirement for sudo
+    sed -i 's/^Defaults[ ]*requiretty/# Defaults requiretty/g' /etc/sudoers
+
+	useradd -c "HPC User" -g $HPC_GROUP -d $SHARE_HOME/$HPC_USER -s /bin/bash -u $HPC_UID $HPC_USER
+
+    chown $HPC_USER:$HPC_GROUP $SHARE_DATA	
+}
 
 # Remove RHUI
 
@@ -350,6 +384,7 @@ allow_passwordssh
     configure_network
     configure_disks
     configure_gluster
+    setup_user
 
 
 
