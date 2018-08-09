7c18a90a50ce1e9926b61eeae53c4c545f11b8e7
Steven Roach <stevenwroach@hotmail.com>
2018-08-09 16:46:02 -0700

    Update gfsclient.sh

--- gfsclient.sh
+++ gfsclient.sh
@@ -6,11 +6,23 @@ GLUSTERHOSTPREFIX=${1}
 GLUSTERHOSTCOUNT=${2}
 GLUSTERVOLUME=${3}
 
+LOGINSERVER=${4}
+
+# Shares
+SHARE_HOME=/share/home
+mkdir -p /share
+mkdir -p $SHARE_HOME
+
+# User
+HPC_USER=hpcuser
+HPC_UID=7007
+HPC_GROUP=hpc
+HPC_GID=7007
+
 MOUNTPOINT=/mnt/${GLUSTERVOLUME}
 mkdir -p ${MOUNTPOINT}
 
-yum -y install epel.release 
-yum -y install sshpass nmap
+yum -y install nfs-utils nfs-utils-lib
 
 #Install Gluster Fuse Client
 
@@ -75,3 +87,25 @@ make install
 # disable selinux
 sed -i 's/^SELINUX=.*/SELINUX=disabled/I' /etc/selinux/config
 setenforce 0
+
+
+  
+
+	echo "$LOGINSERVER:$SHARE_HOME $SHARE_HOME    nfs4    rw,auto,_netdev 0 0" >> /etc/fstab
+	mount -a
+	mount
+   
+    groupadd -g $HPC_GID $HPC_GROUP
+
+    # Don't require password for HPC user sudo
+    echo "$HPC_USER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+    
+    # Disable tty requirement for sudo
+    sed -i 's/^Defaults[ ]*requiretty/# Defaults requiretty/g' /etc/sudoers
+
+	useradd -c "HPC User" -g $HPC_GROUP -d $SHARE_HOME/$HPC_USER -s /bin/bash -u $HPC_UID $HPC_USER
+
+    chown $HPC_USER:$HPC_GROUP $SHARE_DATA	
+
+
+
