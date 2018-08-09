31b11c09cfc56c6eb38ce3de4aa4c43efa0a2315
Steven Roach <stevenwroach@hotmail.com>
2018-08-09 16:37:37 -0700

    Update login.sh

--- login.sh
+++ login.sh
@@ -1,1 +1,99 @@
 #!/bin/bash
+
+set -x
+
+if [[ $(id -u) -ne 0 ]] ; then
+    echo "Must be run as root"
+    exit 1
+fi
+
+
+# Shares
+SHARE_HOME=/share/home
+
+
+# User
+HPC_USER=hpcuser
+HPC_UID=7007
+HPC_GROUP=hpc
+HPC_GID=7007
+
+MASTER_NAME=`hostname`
+
+
+
+# Installs all required packages.
+#
+install_pkgs()
+{
+	yum -y install nfs-utils nfs-utils-lib
+}
+
+
+
+setup_nfs()
+{
+    mkdir -p $SHARE_HOME
+
+    echo "$SHARE_HOME  *(rw,async)" >> /etc/exports
+    
+    systemctl enable rpcbind || echo "Already enabled"
+    systemctl enable nfs-server || echo "Already enabled"
+    systemctl start rpcbind || echo "Already enabled"
+    systemctl start nfs-server || echo "Already enabled"
+	exportfs
+	exportfs -a
+	exportfs 
+}
+
+
+setup_user()
+{
+    # disable selinux
+    sed -i 's/enforcing/disabled/g' /etc/selinux/config
+    setenforce permissive
+    
+    groupadd -g $HPC_GID $HPC_GROUP
+    
+    # Don't require password for HPC user sudo
+    echo "$HPC_USER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+    
+    # Disable tty requirement for sudo
+    sed -i 's/^Defaults[ ]*requiretty/# Defaults requiretty/g' /etc/sudoers
+   
+	useradd -c "HPC User" -g $HPC_GROUP -m -d $SHARE_HOME/$HPC_USER -s /bin/bash -u $HPC_UID $HPC_USER
+
+	mkdir -p $SHARE_HOME/$HPC_USER/.ssh
+
+	# Configure public key auth for the HPC user
+	ssh-keygen -t rsa -f $SHARE_HOME/$HPC_USER/.ssh/id_rsa -q -P ""
+	cat $SHARE_HOME/$HPC_USER/.ssh/id_rsa.pub >> $SHARE_HOME/$HPC_USER/.ssh/authorized_keys
+
+	echo "Host *" > $SHARE_HOME/$HPC_USER/.ssh/config
+	echo "    StrictHostKeyChecking no" >> $SHARE_HOME/$HPC_USER/.ssh/config
+	echo "    UserKnownHostsFile /dev/null" >> $SHARE_HOME/$HPC_USER/.ssh/config
+	echo "    PasswordAuthentication no" >> $SHARE_HOME/$HPC_USER/.ssh/config
+
+	# Fix .ssh folder ownership
+	chown -R $HPC_USER:$HPC_GROUP $SHARE_HOME/$HPC_USER
+
+	# Fix permissions
+	chmod 700 $SHARE_HOME/$HPC_USER/.ssh
+	chmod 644 $SHARE_HOME/$HPC_USER/.ssh/config
+	chmod 644 $SHARE_HOME/$HPC_USER/.ssh/authorized_keys
+	chmod 600 $SHARE_HOME/$HPC_USER/.ssh/id_rsa
+	chmod 644 $SHARE_HOME/$HPC_USER/.ssh/id_rsa.pub
+	
+	# chown -R $HPC_USER:$HPC_GROUP $NFS_DATA
+}
+
+
+
+install_pkgs
+setup_nfs
+setup_user
+
+
+exit 0
+
+
