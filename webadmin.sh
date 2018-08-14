8c9611133c4532ef80bc8211ff8121bae99584a7
Steven Roach <stevenwroach@hotmail.com>
2018-08-13 22:32:58 -0700

    Update webadmin.sh

--- webadmin.sh
+++ webadmin.sh
@@ -1,13 +1,52 @@
 #!/bin/bash
+set -x
 
+#disable_selinux
+sed -i 's/^SELINUX=.*/SELINUX=disabled/I' /etc/selinux/config
+setenforce 0
 
-subscription-manager register --username="sroach-msft2" --password="Jan@Roach1957"
+adminUsername=${1}
+adminPassword=${2}
+PEERNODEPREFIX=${3}
+NODEINDEX=${4}
+NODECOUNT=${5}
+USERNAME_ORG=${6}
+PASSWORD_ACT_KEY="${7}"
+
+# Remove RHUI
+
+rm -f /etc/yum.repos.d/rh-cloud.repo
+sleep 10
+
+# Register Host with Cloud Access Subscription
+echo $(date) " - Register host with Cloud Access Subscription"
+
+subscription-manager register --username="$USERNAME_ORG" --password="$PASSWORD_ACT_KEY" --auto-attach --force || subscription-manager register --activationkey="$PASSWORD_ACT_KEY" --org="$USERNAME_ORG" --auto-attach  --force
+
+if [ $? -eq 0 ]
+then
+    echo "Subscribed successfully"
+elif [ $? -eq 64 ]
+then
+    echo "This system is already registered."
+else
+    echo "Incorrect Username / Password or Organization ID / Activation Key specified"
+    exit 3
+fi
+
+install_tendrl() {
+
 subscription-manager attach --pool=8a85f9875f7334a1015f74b57adf779e
 subscription-manager repos --disable="*" 
 subscription-manager repos --enable=rhel-7-server-rpms
 subscription-manager repos --enable=rh-gluster-3-web-admin-server-for-rhel-7-server-rpms
 
+yum -y install ansible tendrl-ansible
 
+}
+
+open_ports() {
+   
     firewall-cmd --permanent --zone=public --add-port=2379/tcp
     firewall-cmd --permanent --zone=public --add-port=2003/tcp
     firewall-cmd --permanent --zone=public --add-port=10080/tcp
@@ -16,15 +55,64 @@ subscription-manager repos --enable=rh-gluster-3-web-admin-server-for-rhel-7-ser
     firewall-cmd --permanent --zone=public --add-port=3000/tcp
     firewall-cmd --permanent --zone=public --add-port=8789/tcp   
     firewall-cmd --reload
+}
 
-# vi /etc/yum.repos.d/epel.repo 
+format_disks() {
+fdisk /dev/sdc << EOF
+n
+p
+1
 
-yum -y install ansible tendrl-ansible
+
+t
+83
+w
+EOF
+
+mkfs -t ext4 /dev/sdc1
+mkdir -p /var/lib/etcd
+echo "/dev/sdc1 /var/lib/etcd ext4 defaults,nofail 0 2" >> /etc/fstab
+
+
+fdisk /dev/sdd << EOF
+n
+p
+1
+
+
+t
+83
+w
+EOF
+
+mkfs -t ext4 /dev/sdd1
+mkdir -p /var/lib/carbon
+echo "/dev/sdd1 /var/lib/carbon ext4 defaults,nofail 0 2" >> /etc/fstab
+
+mount -a
+
+}
 
 #edit /etc/ansible/hosts
 # copy site.yml
 # run the playbook
 
+
+
+configure_ssh() {
+    DNSsuffix=$(nslookup `hostname` | grep Name | cut -f 2 | cut -d "." -f 2-)
+
+runuser 
 ssh-copy-id sroach@glusterVM4
 
+}
 
+format_disks
+install_tendrl
+open_ports
+#configure_ssh
+
+
+# edit /etc/ansible/hosts
+# copy site.yml
+# run the playbook
